bd3fa40a3a275338a7a69d7fad003c89ab575382
Zahid Ibrahim Shaikh <53161242+The-Lone-Druid@users.noreply.github.com>
2020-09-25 11:39:38 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,12 +7,30 @@ $(document).ready(function() {
         $('.menu').toggleClass('fa-times');
         $('body').css('overflow-y','hidden');
         
-        $('.overlay').click(function() {
+        $('.overlay , .nav-item').click(function() {
             $('.side-bar').removeClass('display');
             $('.overlay').removeClass('active');
             $('.menu').removeClass('fa-times');
             $('body').css('overflow-y','scroll');
         });
-    })
+    });
 
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+    
+          // Store hash
+          var hash = this.hash;
+    
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 0, function(){
+          });
+        } // End if
+      });
 });
