37c06f68004fe0a6188553c5f54b6cb946bf89d2
Zahid Ibrahim Shaikh <53161242+The-Lone-Druid@users.noreply.github.com>
2020-10-01 15:40:03 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,3 +60,24 @@ $(document).ready(function() {
       }, 600 );
     });
 });
+
+//Form Submission
+$(function(){
+  $(".ajaxForm").submit(function(e){
+      e.preventDefault();
+      var href = $(this).attr("action");
+      $.ajax({
+          type: "POST",
+          dataType: "json",
+          url: href,
+          data: $(this).serialize(),
+          success: function(response){
+              if(response.status == "success"){
+                  window.location.href = 'thanks.html'; // change this. 
+              }else{
+                  alert("An error occured: " + response.message);
+              }
+          }
+      });
+  });
+});
